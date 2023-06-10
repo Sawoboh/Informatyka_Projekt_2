85f3090b46443ea662bd03ba94e6927c75d434ba
Adrian Maksymiuk <adiri12613@gmail.com>
2023-06-10 14:19:57 +0200

    test pole

--- pole.py
+++ pole.py
@@ -1,41 +1,31 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
+Created on Sat Jun 10 13:16:52 2023
 
-This is a temporary script file.
+@author: amaks
 """
 
-K=[[1,1],[1,2],[2,2],[2,1]]
+'''pole powierzchni'''
 
-
-
-if len(K) > 2:
-    suma=0
-    for i in range(len(K)):
-        if i<len(K)-1:
-            P=(K[i][0]*(K[i+1][1]-K[i-1][1]))
-            suma += P
-    P=(K[-1][0]*(K[0][1]-K[-2][1]))
-    suma += P
-    suma=0.5*abs(suma)    
-    print(suma)
-
-X = []
-Y = []
+'''K=[[521912.62,610971.36],[521918.62,610973.36],[521915.62,610977.36]]'''
+K=[[1,1],[3,3],[6,7],[4,4],[2,4]]
+pole=0
 for i in range(len(K)):
-    X.append(K[i][0])
-    Y.append(K[i][1])
-n = len(K)
-pole = 0
-for i in range(n-1):
-    x1 = X[i]
-    y1 = Y[i]
-    x2 = X[i+1]
-    y2 = Y[i+1]
-    pole += x1 * y2 - x2 * y1
-    
-pole /= 2
-pole_m2 = abs(pole)
-poletxt = f"Pole: {pole_m2:.3f} [m2]"
-print(poletxt)
-    
+    x1=K[i][0]
+    y1=K[i][1]
+    x2=K[(i+1) % len(K)][0]
+    y2=K[(i+1) % len(K)][1] 
+    pole += x1*y2 - x2*y1
+print(abs(pole/2))
+
+
+
+suma=0
+for i in range(len(K)):
+    if i<len(K)-1:
+        P=(K[i][0]*(K[i+1][1]-K[i-1][1]))
+        suma += P
+P=(K[-1][0]*(K[0][1]-K[-2][1]))
+suma += P
+suma=0.5*abs(suma)  
+print("%.3f"%suma) 
